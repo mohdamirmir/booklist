e5eeee44c2aeb0f2c2c8e0471e477b514739ae8f
aamirmir111 <aamir.mir111@gmail.com>
2020-09-15 11:33:20 +0400

    Updates

---
+++ precache-manifest.882c427b1eb61ab94b212d1e297eedf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c452f54231f92ea0b3301720f1c05ae",
+    "url": "/booklist/index.html"
+  },
+  {
+    "revision": "534bb4489acd79c9ab9f",
+    "url": "/booklist/static/css/main.360f79e7.chunk.css"
+  },
+  {
+    "revision": "1a06f982f61763b38d74",
+    "url": "/booklist/static/js/2.2e317db2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/booklist/static/js/2.2e317db2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "534bb4489acd79c9ab9f",
+    "url": "/booklist/static/js/main.204f7eed.chunk.js"
+  },
+  {
+    "revision": "f55020a32a73c9d08978",
+    "url": "/booklist/static/js/runtime-main.a768f342.js"
+  }
+]);
